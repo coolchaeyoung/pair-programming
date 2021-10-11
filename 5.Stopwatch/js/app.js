c5576a2a4472a740ae82234ff9a88ee229886377
coolchaeyoung <ggodchae@naver.com>
2021-10-11 21:48:01 +0900

    feat: edit stopwatch

--- 5.Stopwatch/js/app.js
+++ 5.Stopwatch/js/app.js
@@ -8,23 +8,32 @@ const $display = document.querySelector('.display');
 const [$startOrStop, $resetOrLap] = document.querySelectorAll('.control');
 const $laps = document.querySelector('.laps');
 
-const render = () => {
-  const minutes = Math.floor(state.time / 60000);
-  const second = Math.floor((state.time % 60000) / 1000);
-  const millisecond = Math.floor((state.time % 1000) / 10);
-  $display.textContent = `${minutes < 10 ? '0' + minutes : minutes}:${
+const timeToString = time => {
+  const minutes = Math.floor(time / 60000);
+  const second = Math.floor((time % 60000) / 1000);
+  const millisecond = Math.floor((time % 1000) / 10);
+  return `${minutes < 10 ? '0' + minutes : minutes}:${
     second < 10 ? '0' + second : second
   }:${millisecond < 10 ? '0' + millisecond : millisecond}`;
+};
+
+const render = () => {
+  $display.textContent = timeToString(state.time);
 
   $startOrStop.textContent = state.isStart ? 'Stop' : 'Start';
 
   $resetOrLap.toggleAttribute('disabled', !state.time);
   $resetOrLap.textContent = state.isStart ? 'Lap' : 'Reset';
 
-  // const $fragment = document.createDocumentFragment();
-  $laps.innerHTML = state.laps
-    .map(({ lap, time }) => `<div>${lap}</div><div>${time}</div>`)
-    .join('');
+  $laps.innerHTML = `
+    <div class="lap-title">Laps</div>
+    <div class="lap-title">Time</div>
+    ${state.laps
+      .map(
+        ({ lap, time }) => `<div>${lap}</div><div>${timeToString(time)}</div>`
+      )
+      .join('')}
+  `;
 };
 
 const setState = newState => {
