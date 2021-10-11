e9fb03fc342dc8890ec330a872fddcc7cf18ba83
coolchaeyoung <ggodchae@naver.com>
2021-10-11 19:22:11 +0900

    feat: Add Stopwatch

--- 5.Stopwatch/js/app.js
+++ 5.Stopwatch/js/app.js
@@ -1,12 +1,12 @@
-// state
 let state = {
   time: 0,
-  labs: [],
+  laps: [],
   isStart: false
 };
 
 const $display = document.querySelector('.display');
-const $start = document.querySelector('.control');
+
+const [$startOrStop, $resetOrLap] = document.querySelectorAll('.control');
 
 const render = () => {
   const minutes = Math.floor(state.time / 60000);
@@ -15,6 +15,11 @@ const render = () => {
   $display.textContent = `${minutes < 10 ? '0' + minutes : minutes}:${
     second < 10 ? '0' + second : second
   }:${millisecond < 10 ? '0' + millisecond : millisecond}`;
+
+  $startOrStop.textContent = state.isStart ? 'Stop' : 'Start';
+
+  $resetOrLap.toggleAttribute('disabled', !state.isStart);
+  $resetOrLap.textContent = state.isStart ? 'Lap' : 'Reset';
 };
 
 const setState = newState => {
@@ -22,10 +27,25 @@ const setState = newState => {
   render();
 };
 
-$start.addEventListener('click', e => {
+$startOrStop.addEventListener('click', () => {
   const standard = new Date();
   const timerId = setInterval(() => {
+    if (state.isStart) {
+      clearInterval(timerId);
+      setState({ ...this.state, isStart: false });
+    }
     const time = new Date() - standard;
     setState({ ...state, time, isStart: true });
   }, 10);
+});
+
+$resetOrLap.addEventListener('click', () => {
+  if (state.isStart) {
+    setState({});
+  } else {
+    setState({
+      ...this.state,
+      laps: [...this.laps, { lap: state.laps.length + 1, time: state.time }]
+    });
+  }
 });
