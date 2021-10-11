fa428f4f4fb576c882eeb22548121697f83ac865
joker77z <joker77z@naver.com>
2021-10-11 14:45:05 +0900

    feat: Add 1 app.js
    add app.js

--- 1.Toggle side nav/js/app.js
+++ 1.Toggle side nav/js/app.js
@@ -1,1 +1,27 @@
-console.log('test');
+// state
+let isToggle = false;
+
+const $nav = document.querySelector('nav');
+const $toggle = document.querySelector('.toggle');
+
+const render = () => {
+  $nav.classList.toggle('active', isToggle);
+};
+
+const setIsToggle = newIsToggle => {
+  isToggle = newIsToggle;
+  // 로컬스토리지에 isToggle 저장.
+  localStorage.setItem('isToggle', isToggle);
+  render();
+};
+
+$toggle.addEventListener('click', () => {
+  setIsToggle(!isToggle);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const localStorageValue =
+    JSON.parse(localStorage.getItem('isToggle')) || false;
+  console.log(typeof localStorageValue, localStorageValue);
+  setIsToggle(localStorageValue);
+});
