6cc0261d625cf9e41f1a177026362c243b5fed09
joker77z <joker77z@naver.com>
2021-10-12 13:40:51 +0900

    feat: Add 7 popup
    add pop

---
+++ 7.Popup/js/app.js
@@ -0,0 +1,47 @@
+let state = {
+  isPopup: false,
+  textList: []
+};
+
+const $togglePopup = document.querySelector('.toggle-popup');
+const $popupMessage = document.querySelector('.popup-message');
+const $modalContainer = document.querySelector('.modal-container');
+const $modalInput = document.querySelector('.modal-input');
+
+const render = () => {
+  $popupMessage.innerHTML = state.textList
+    .map(text => `<div>${text}</div>`)
+    .join('');
+  $modalContainer.classList.toggle('active', state.isPopup);
+};
+const setState = newState => {
+  state = newState;
+  render();
+};
+
+const addText = newText => {
+  setState({ ...state, textList: [newText, ...state.textList] });
+};
+
+$togglePopup.addEventListener('click', () => {
+  setState({ ...state, isPopup: true });
+});
+
+$modalContainer.addEventListener('click', e => {
+  if (e.target.classList.contains('confirm-button')) {
+    const content = $modalInput.value.trim();
+    if (content) addText(content);
+    $modalInput.value = '';
+  }
+  if (e.target.closest('.button') || !e.target.closest('.modal')) {
+    setState({ ...state, isPopup: false });
+  }
+});
+
+$modalInput.addEventListener('keyup', e => {
+  if (e.key === 'Enter') {
+    const content = $modalInput.value.trim();
+    if (content) addText(content);
+    $modalInput.value = '';
+  }
+});
