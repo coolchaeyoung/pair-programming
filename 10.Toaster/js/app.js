3276547c300720d95b343842623b22385c421e7a
coolchaeyoung <ggodchae@naver.com>
2021-10-12 20:04:48 +0900

    feat: Add Toaster

--- 10.Toaster/js/app.js
+++ 10.Toaster/js/app.js
@@ -1,35 +1,63 @@
 const toaster = (function () {
-  let toasterList = [];
+  let state = {
+    toasterList: [],
+    isShowState: false
+  };
 
   const render = () => {
-    const $fragment = document.createDocumentFragment();
+    const $body = document.querySelector('body');
+    const $toasts = document.querySelectorAll('.toast');
 
-    toasterList.forEach(({ type, title, message }) => {
+    if (state.isShowState) {
+      const $fragment = document.createDocumentFragment();
+      [...$toasts].forEach(($toast, i, $toasts) => {
+        $toast.style.bottom = ($toasts.length - i) * 100 + 'px';
+      });
+
+      const { type, title, message } =
+        state.toasterList[state.toasterList.length - 1];
       const $div = document.createElement('div');
       $div.className = `toast toast-${type}`;
+      $div.style.bottom = 0;
       $div.innerHTML = `
-        <h4 class="toast-heading">${title}</h4>
-        <div class="toast-message">
-          <svg width="24" height="24">
-           <use xlink:href="#${type}" />
-          </svg>
-        <p>${message}</p>
-        </div>
-        <a class="close">&times;</a>`;
+          <h4 class="toast-heading">${title}</h4>
+          <div class="toast-message">
+            <svg width="24" height="24">
+             <use xlink:href="#${type}" />
+            </svg>
+          <p>${message}</p>
+          </div>
+          <a class="close">&times;</a>`;
 
       $fragment.appendChild($div);
-    });
-    const $body = document.querySelector('body');
-    $body.appendChild($fragment);
+
+      $body.appendChild($fragment);
+    } else {
+      const $toast = document.querySelector('.toast');
+      $body.removeChild($toast);
+    }
   };
-  const setToasterList = newToasterList => {
-    toasterList = newToasterList;
+
+  const setState = newState => {
+    state = newState;
+    console.log(state.toasterList);
     render();
   };
 
   return {
     add: newToaster => {
-      setToasterList([...toasterList, newToaster]);
+      setState({
+        ...state,
+        toasterList: [...state.toasterList, newToaster],
+        isShowState: true
+      });
+      setTimeout(() => {
+        setState({
+          ...state,
+          toasterList: state.toasterList.slice(1),
+          isShowState: false
+        });
+      }, 3000);
     }
   };
 })();
