324ca444c1f8954522a181f912d9dfb1f6de96b2
joker77z <joker77z@naver.com>
2021-10-12 19:07:39 +0900

    feat: toaster tmp
    tmp

--- 10.Toaster/js/app.js
+++ 10.Toaster/js/app.js
@@ -1,4 +1,38 @@
-const toaster = 1;
+const toaster = (function () {
+  let toasterList = [];
+
+  const render = () => {
+    const $fragment = document.createDocumentFragment();
+
+    toasterList.forEach(({ type, title, message }) => {
+      const $div = document.createElement('div');
+      $div.className = `toast toast-${type}`;
+      $div.innerHTML = `
+        <h4 class="toast-heading">${title}</h4>
+        <div class="toast-message">
+          <svg width="24" height="24">
+           <use xlink:href="#${type}" />
+          </svg>
+        <p>${message}</p>
+        </div>
+        <a class="close">&times;</a>`;
+
+      $fragment.appendChild($div);
+    });
+    const $body = document.querySelector('body');
+    $body.appendChild($fragment);
+  };
+  const setToasterList = newToasterList => {
+    toasterList = newToasterList;
+    render();
+  };
+
+  return {
+    add: newToaster => {
+      setToasterList([...toasterList, newToaster]);
+    }
+  };
+})();
 
 const TOAST_TYPE = {
   SUCCESS: 'success',
