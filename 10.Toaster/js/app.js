baa2fb4abfa144f1d811fbeb70ec7d276ef24bf6
coolchaeyoung <ggodchae@naver.com>
2021-10-14 16:37:12 +0900

    refactor: 10.Toaster

--- 10.Toaster/js/app.js
+++ 10.Toaster/js/app.js
@@ -1,47 +1,56 @@
 const toaster = (function () {
   let state = {
     toasterList: [],
-    isShowState: false
+    isAdd: false
   };
 
+  const $body = document.querySelector('body');
+
   const render = () => {
-    const $body = document.querySelector('body');
-    const $toasts = document.querySelectorAll('.toast');
-
-    if (state.isShowState) {
-      const $fragment = document.createDocumentFragment();
-      [...$toasts].forEach(($toast, i, $toasts) => {
-        $toast.style.bottom = ($toasts.length - i) * 100 + 'px';
-      });
-
-      const { type, title, message } =
-        state.toasterList[state.toasterList.length - 1];
-      const $div = document.createElement('div');
-      $div.className = `toast toast-${type}`;
-      $div.style.bottom = 0;
-      $div.innerHTML = `
-          <h4 class="toast-heading">${title}</h4>
-          <div class="toast-message">
-            <svg width="24" height="24">
-             <use xlink:href="#${type}" />
-            </svg>
-          <p>${message}</p>
-          </div>
-          <a class="close">&times;</a>`;
-
-      $fragment.appendChild($div);
-
-      $body.appendChild($fragment);
-    } else {
+    if (!state.isAdd) {
       const $toast = document.querySelector('.toast');
       $body.removeChild($toast);
+      return;
     }
+
+    const { type, title, message } =
+      state.toasterList[state.toasterList.length - 1];
+
+    const $div = document.createElement('div');
+    $div.className = `toast toast-${type}`;
+    $div.innerHTML = `
+        <h4 class="toast-heading">${title}</h4>
+        <div class="toast-message">
+          <svg width="24" height="24">
+            <use xlink:href="#${type}" />
+          </svg>
+        <p>${message}</p>
+        </div>
+        <a class="close">&times;</a>`;
+
+    $body.appendChild($div);
+
+    const $toasts = document.querySelectorAll('.toast');
+    const toastHeight = +window
+      .getComputedStyle($toasts[0])
+      .getPropertyValue('--toast-height')
+      .replace('px', '');
+    [...$toasts].forEach(($toast, i, $toasts) => {
+      $toast.style.bottom = ($toasts.length - (i + 1)) * toastHeight + 'px';
+    });
   };
 
   const setState = newState => {
     state = newState;
-    console.log(state.toasterList);
     render();
+  };
+
+  const remove = () => {
+    setState({
+      ...state,
+      toasterList: state.toasterList.slice(1),
+      isAdd: false
+    });
   };
 
   return {
@@ -49,15 +58,10 @@ const toaster = (function () {
       setState({
         ...state,
         toasterList: [...state.toasterList, newToaster],
-        isShowState: true
+        isAdd: true
       });
-      setTimeout(() => {
-        setState({
-          ...state,
-          toasterList: state.toasterList.slice(1),
-          isShowState: false
-        });
-      }, 3000);
+
+      setTimeout(remove, 3000);
     }
   };
 })();
