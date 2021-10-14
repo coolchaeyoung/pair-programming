e2125f4fff585eed33e41ad2ca77e8ceb5b17df3
coolchaeyoung <ggodchae@naver.com>
2021-10-14 11:09:29 +0900

    refactor: 5.Stopwatch

--- 5.Stopwatch/js/app.js
+++ 5.Stopwatch/js/app.js
@@ -1,7 +1,7 @@
 let state = {
   time: 0,
   laps: [],
-  isStart: false
+  isRunning: false
 };
 
 const $display = document.querySelector('.display');
@@ -12,18 +12,18 @@ const timeToString = time => {
   const minutes = Math.floor(time / 60000);
   const second = Math.floor((time % 60000) / 1000);
   const millisecond = Math.floor((time % 1000) / 10);
-  return `${minutes < 10 ? '0' + minutes : minutes}:${
-    second < 10 ? '0' + second : second
-  }:${millisecond < 10 ? '0' + millisecond : millisecond}`;
+
+  const format = n => (n < 10 ? '0' + n : n + '');
+  return `${format(minutes)}:${format(second)}:${format(millisecond)}`;
 };
 
 const render = () => {
   $display.textContent = timeToString(state.time);
 
-  $startOrStop.textContent = state.isStart ? 'Stop' : 'Start';
+  $startOrStop.textContent = state.isRunning ? 'Stop' : 'Start';
 
   $resetOrLap.toggleAttribute('disabled', !state.time);
-  $resetOrLap.textContent = state.isStart ? 'Lap' : 'Reset';
+  $resetOrLap.textContent = state.isRunning ? 'Lap' : 'Reset';
 
   $laps.innerHTML = `
     <div class="lap-title">Laps</div>
@@ -41,28 +41,32 @@ const setState = newState => {
   render();
 };
 
+const start = () => {
+  setState({ ...state, isRunning: true });
+  const standardTime = new Date().getTime() - state.time;
+
+  const timerId = setInterval(() => {
+    if (!state.isRunning) clearInterval(timerId);
+
+    const time = new Date() - standardTime;
+    setState({ ...state, time });
+  }, 10);
+};
+
+const stop = () => {
+  setState({ ...state, isRunning: false });
+};
+
 $startOrStop.addEventListener('click', () => {
-  const standard = new Date().getTime() - state.time;
-  let timerId;
-  if (state.isStart) {
-    setState({ ...state, isStart: false });
-  } else {
-    setState({ ...state, isStart: true });
-    timerId = setInterval(() => {
-      if (!state.isStart) clearInterval(timerId);
-      const time = new Date() - standard;
-      setState({ ...state, time });
-    }, 10);
-  }
+  state.isRunning ? stop() : start();
 });
 
 $resetOrLap.addEventListener('click', () => {
-  if (state.isStart) {
-    setState({
-      ...state,
-      laps: [...state.laps, { lap: state.laps.length + 1, time: state.time }]
-    });
-  } else {
-    setState({ time: 0, laps: [], isStart: false });
-  }
+  const laps = state.isRunning
+    ? [...state.laps, { lap: state.laps.length + 1, time: state.time }]
+    : [];
+
+  state.isRunning
+    ? setState({ ...state, laps })
+    : setState({ time: 0, laps, isRunning: false });
 });
