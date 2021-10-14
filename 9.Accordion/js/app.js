851519cde61642b050e22ad0572b0770e22791a5
coolchaeyoung <ggodchae@naver.com>
2021-10-14 16:37:05 +0900

    refactor: 9.Accordion

--- 9.Accordion/js/app.js
+++ 9.Accordion/js/app.js
@@ -1,21 +1,18 @@
-const $accordion = document.querySelector('.accordion');
 let select = '';
 
-const render = () => {
-  [...$accordion.querySelectorAll('.menu-container')].forEach(
-    $menuContainer => {
-      const menuText = $menuContainer.querySelector('.menu').textContent.trim();
-      const toggleCondition =
-        menuText === select
-          ? !$menuContainer.classList.contains('.active')
-          : false;
-      $menuContainer.classList.toggle('active', toggleCondition);
+const $accordion = document.querySelector('.accordion');
+const $menuContainers = document.querySelectorAll('.menu-container');
 
-      const $submenu = $menuContainer.querySelector('.submenu');
-      $submenu.style.height =
-        menuText === select ? $submenu.scrollHeight + 'px' : 0;
-    }
-  );
+const render = () => {
+  [...$menuContainers].forEach($menuContainer => {
+    const menuText = $menuContainer.querySelector('.menu').textContent.trim();
+
+    $menuContainer.classList.toggle('active', select === menuText);
+
+    const $submenu = $menuContainer.querySelector('.submenu');
+    $submenu.style.height =
+      menuText === select ? $submenu.scrollHeight + 'px' : 0;
+  });
 };
 
 const setSelect = newSelect => {
@@ -25,6 +22,8 @@ const setSelect = newSelect => {
 
 $accordion.addEventListener('click', e => {
   const $menu = e.target.closest('.menu');
+  if (!$menu) return;
+
   const newSelect = $menu.textContent.trim();
   setSelect(newSelect);
 });
