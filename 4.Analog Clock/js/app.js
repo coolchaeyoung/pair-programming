a111e77d2242fb5a2ac2fddfd57b1539466213e5
coolchaeyoung <ggodchae@naver.com>
2021-10-15 21:45:04 +0900

    refactor: 4.Analog Clock

--- 4.Analog Clock/js/app.js
+++ 4.Analog Clock/js/app.js
@@ -1,3 +1,9 @@
+const MOVE_PER = {
+  HOUR: time => 30 * time.hour + 0.5 * time.minute,
+  MINUTE: time => 6 * time.minute + 0.1 * time.second,
+  SECOND: time => 6 * time.second
+};
+
 let state = {
   hour: 0,
   minute: 0,
@@ -9,12 +15,6 @@ const $minute = document.querySelector('.minute');
 const $second = document.querySelector('.second');
 
 const render = () => {
-  const MOVE_PER = {
-    HOUR: time => 30 * time.hour + 0.5 * time.minute,
-    MINUTE: time => 6 * time.minute + 0.1 * time.second,
-    SECOND: time => 6 * time.second
-  };
-
   $hour.style.setProperty('--deg', MOVE_PER.HOUR(state));
   $minute.style.setProperty('--deg', MOVE_PER.MINUTE(state));
   $second.style.setProperty('--deg', MOVE_PER.SECOND(state));
